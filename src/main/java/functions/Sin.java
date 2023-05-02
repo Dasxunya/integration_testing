a4f596e8549eb8cd309066c21b8f6c63d39c88aa
Dasxunya <79106085+Dasxunya@users.noreply.github.com>
2023-05-02 21:08:00 +0300

    fx

--- src/main/java/functions/Sin.java
+++ src/main/java/functions/Sin.java
@@ -3,6 +3,9 @@ package functions;
 import utils.AccuracyException;
 import utils.Calculatable;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Sin implements Calculatable {
     public double calculate(double x, double eps) throws AccuracyException, ArithmeticException {
         if (eps <= 0 || eps >= 1)
@@ -16,16 +19,22 @@ public class Sin implements Calculatable {
         else if (Math.abs(x) == Math.PI / 2) return 1;
         else if (Math.abs(x) == Math.PI / 2 * 3) return -1;
 
-        double result = 0;
-        double term = x;
+        BigDecimal result = BigDecimal.ZERO;
+        BigDecimal term = BigDecimal.valueOf(x);
         int n = 1;
 
-        while (Math.abs(term) >= eps) {
-            result += term;
-            term = -term * x * x / (2 * n) / (2 * n + 1);
+        do{
+            result = result.add(term);
+            term = term.multiply(BigDecimal.valueOf(-1 * x * x)).divide(BigDecimal.valueOf(2 * n), 30, RoundingMode.HALF_UP).divide(BigDecimal.valueOf(2 * n + 1), 30, RoundingMode.HALF_UP);
             n++;
-        }
-        return result;
+        } while(term.abs().compareTo(BigDecimal.valueOf(eps)) >= 0);
+
+//        while (Math.abs(term) >= eps) {
+//            result += term;
+//            term = -term * x * x / (2 * n) / (2 * n + 1);
+//            n++;
+//        }
+        return result.doubleValue();
     }
 
     public static double normalizeAngle(double x) {
